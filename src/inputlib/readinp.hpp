ed092595a249c56b340660fff47b731b5730e379
fabian0hofmann <fabian.l.hofmann@gmx.de>
2026-09-18 05:23:44 +0200

    populated readinp.hpp

--- src/inputlib/readinp.hpp
+++ src/inputlib/readinp.hpp
@@ -2,7 +2,37 @@
 // Created by fabian on 18.09.26.
 //
 
-#ifndef FUN_USING_COMPUTATIONAL_CHEMISTRY_READINP_HPP
-#define FUN_USING_COMPUTATIONAL_CHEMISTRY_READINP_HPP
+#ifndef READINP_HPP
+#define READINP_HPP
 
-#endif //FUN_USING_COMPUTATIONAL_CHEMISTRY_READINP_HPP
+#include <string>
+#include <vector>
+#include <map>
+
+struct Keyword {
+    std::string keyword;
+    std::vector<std::string> raw_input;
+    std::map<std::string, std::vector<std::string>> parameters;
+};
+
+class Input {
+
+    public:
+
+        explicit Input(const std::string& filename);
+
+        [[nodiscard]] const Keyword* get_keyword(const std::string& module_name) const;
+
+    private:
+        std::vector<Keyword> keywords_;
+        std::map<std::string, size_t> keyword_index_;
+
+        static std::string trim(const std::string& s);
+        static std::vector<std::string> tokenize(const std::string& s);
+
+        void parse(const std::string& filename);
+};
+
+Input read_input_file();
+
+#endif //READINP_HPP
