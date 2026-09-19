adde070d535e61a983f36d66b0bcbca3b443f778
fabian0hofmann <fabian.l.hofmann@gmx.de>
2026-09-19 13:44:37 +0200

    put read_matrix() into huckel_driver.cpp

--- src/huckellib/huckel_driver.cpp
+++ src/huckellib/huckel_driver.cpp
@@ -7,7 +7,25 @@ int main() {
 
     const auto input = read_input_file();
 
-    const auto huckel_matrix = read_matrix();
+    const Keyword* huckel = input.get_keyword("huckel");
+    if (!huckel){
+        std::cerr << "Fatal: Tried to run Hückel module without $huckel keyword in the input file." << std::endl;
+        return 1;
+    }
+
+    std::string matrix_filename = "matrix";
+
+    const auto matrix_it = huckel->parameters.find("matrix");
+    if (matrix_it == huckel->parameters.end() || matrix_it->second.empty()){
+        std::cout << "\nNo matrix file name specified, defaulting to \"matrix\"." << "\n";
+    }else{
+        matrix_filename = matrix_it->second[0];
+    }
+
+    const auto huckel_matrix = read_matrix(matrix_filename);
+
+    std::cout << "\nRead Hückel matrix:\n"
+              << huckel_matrix << "\n\n";
 
     std::string debug_string;
     std::ifstream input_file("job");
