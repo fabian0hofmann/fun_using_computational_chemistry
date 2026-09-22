f2ebe7d4e0599e1159df08b1855540698980ae42
fabian0hofmann <fabian.l.hofmann@gmx.de>
2026-09-22 05:05:47 +0200

    populated diagmat.cpp

--- src/huckellib/diagmat.cpp
+++ src/huckellib/diagmat.cpp
@@ -1,7 +1,10 @@
 #include "diagmat.hpp"
 
-#include <fstream>
-#include <sstream>
-#include <stdexcept>
-#include <vector>
+Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> diag_huckel_matrix(const Eigen::MatrixXd& huckel_matrix){
+    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> huckel_solver(huckel_matrix);
+    if (huckel_solver.info() != Eigen::Success){
+        throw std::logic_error("Diagonalization of Hückel matrix failed.");
+    }
+    return huckel_solver;
+}
 
