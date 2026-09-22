8309a5c894ff295c43c803e50487a726e4266158
fabian0hofmann <fabian.l.hofmann@gmx.de>
2026-09-22 05:05:53 +0200

    populated diagmat.hpp

--- src/huckellib/diagmat.hpp
+++ src/huckellib/diagmat.hpp
@@ -6,8 +6,7 @@
 #define FUN_USING_COMPUTATIONAL_CHEMISTRY_DIAGMAT_HPP
 
 #include <Eigen/Dense>
-#include <string>
 
-Eigen::MatrixXd diag_huckel_matrix(const Eigen::MatrixXd& huckel_matrix);
+Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> diag_huckel_matrix(const Eigen::MatrixXd& huckel_matrix);
 
 #endif //FUN_USING_COMPUTATIONAL_CHEMISTRY_DIAGMAT_HPP
