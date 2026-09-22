a2a2bd7bbedd48ec44348946bfb074d87c394c11
fabian0hofmann <fabian.l.hofmann@gmx.de>
2026-09-22 05:23:54 +0200

    wrote huckel_output()

--- src/huckellib/huckelout.cpp
+++ src/huckellib/huckelout.cpp
@@ -1,3 +1,17 @@
-//
-// Created by fabian on 22.09.26.
-//
+#include "huckelout.hpp"
+
+void huckel_output(Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd>& huckel_solver){
+
+    std::cout << "\n\n";
+    std::cout << "#################################################################################\n";
+    std::cout << "#                                                                               #\n";
+    std::cout << "#                            HÜCKEL CALCULATION RESULT                          #\n";
+    std::cout << "#                                                                               #\n";
+    std::cout << "#################################################################################\n\n\n" << std::endl;
+    std::cout << "----- Eigenvalues -----\n\n";
+    std::cout << huckel_solver.eigenvalues() << "\n\n";
+    std::cout << "----- Eigenvectors (orbitals) -----\n\n";
+    std::cout << huckel_solver.eigenvectors() << "\n\n";
+
+    std::cout << "END OF OUTPUT" << std::endl;
+}
